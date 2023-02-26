71a6ab4dabb24b3cb34fb635f1ae1284c494867d
webfolderio <support@webfolder.io>
2023-02-26 10:09:54 +0100

    Update build.sh
    Applied https://www.shellcheck.net/ fixes

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@
 # March 15, 2021
 #
 mkdir build-wget-webfolder.io
-cd build-wget-webfolder.io
+cd build-wget-webfolder.io || exit
 mkdir install
 export INSTALL_PATH=$PWD/install
 export WGET_GCC=x86_64-w64-mingw32-gcc
@@ -15,14 +15,14 @@ export MINGW_STRIP_TOOL=x86_64-w64-mingw32-strip
 # -----------------------------------------------------------------------------
 # build gmp
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libgmp.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libgmp.a ]; then
   wget -nc https://gmplib.org/download/gmp/gmp-6.2.1.tar.xz
   tar -xf gmp-6.2.1.tar.xz
-  cd gmp-6.2.1
+  cd gmp-6.2.1 || exit
   ./configure \
    --host=$WGET_MINGW_HOST \
    --disable-shared \
-   --prefix=$INSTALL_PATH
+   --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[gmp] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[gmp] make failed"; exit 1; }
@@ -33,17 +33,17 @@ fi
 # -----------------------------------------------------------------------------
 # build nettle
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libnettle.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libnettle.a ]; then
   wget -nc https://ftp.gnu.org/gnu/nettle/nettle-3.8.tar.gz
   tar -xf nettle-3.8.tar.gz
-  cd nettle-3.8
+  cd nettle-3.8 || exit
   CFLAGS="-I$INSTALL_PATH/include" \
   LDFLAGS="-L$INSTALL_PATH/lib" \
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
   --disable-documentation \
-  --prefix=$INSTALL_PATH
+  --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[nettle] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[nettle] make failed"; exit 1; }
@@ -54,15 +54,15 @@ fi
 # -----------------------------------------------------------------------------
 # build tasn
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libtasn1.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libtasn1.a ]; then
   wget -nc https://ftp.gnu.org/gnu/libtasn1/libtasn1-4.18.0.tar.gz
   tar -xf libtasn1-4.18.0.tar.gz
-  cd libtasn1-4.18.0
+  cd libtasn1-4.18.0 || exit
   ./configure \
    --host=$WGET_MINGW_HOST \
    --disable-shared \
    --disable-doc \
-   --prefix=$INSTALL_PATH
+   --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[tasn] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[tasn] make failed"; exit 1; }
@@ -73,15 +73,15 @@ fi
 # -----------------------------------------------------------------------------
 # build idn2
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libidn2.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libidn2.a ]; then
   wget -nc https://ftp.gnu.org/gnu/libidn/libidn2-2.3.0.tar.gz
   tar -xf libidn2-2.3.0.tar.gz
-  cd libidn2-2.3.0
+  cd libidn2-2.3.0 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
   --disable-doc \
-  --prefix=$INSTALL_PATH
+  --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[idn2] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[idn2] make failed"; exit 1; }
@@ -92,14 +92,14 @@ fi
 # -----------------------------------------------------------------------------
 # build unistring
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libunistring.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libunistring.a ]; then
   wget -nc https://ftp.gnu.org/gnu/libunistring/libunistring-1.0.tar.gz
   tar -xf libunistring-1.0.tar.gz
-  cd libunistring-1.0
+  cd libunistring-1.0 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH
+  --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[unistring] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[unistring] make failed"; exit 1; }
@@ -110,10 +110,10 @@ fi
 # -----------------------------------------------------------------------------
 # build gnutls
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libgnutls.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libgnutls.a ]; then
   wget -nc https://www.gnupg.org/ftp/gcrypt/gnutls/v3.7/gnutls-3.7.6.tar.xz
   tar -xf gnutls-3.7.6.tar.xz
-  cd gnutls-3.7.6
+  cd gnutls-3.7.6 || exit
   PKG_CONFIG_PATH="$INSTALL_PATH/lib/pkgconfig" \
   CFLAGS="-I$INSTALL_PATH/include" \
   LDFLAGS="-L$INSTALL_PATH/lib" \
@@ -129,7 +129,7 @@ if [ ! -f $INSTALL_PATH/lib/libgnutls.a ]; then
   LIBIDN2_CFLAGS=$CFLAGS \
   ./configure \
   --host=$WGET_MINGW_HOST \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --with-included-unistring \
   --disable-openssl-compatibility \
   --without-p11-kit \
@@ -147,15 +147,15 @@ fi
 # -----------------------------------------------------------------------------
 # build cares
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libcares.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libcares.a ]; then
   wget -nc https://github.com/c-ares/c-ares/releases/download/cares-1_17_2/c-ares-1.17.2.tar.gz
   tar -xf c-ares-1.17.2.tar.gz
-  cd c-ares-1.17.2
+  cd c-ares-1.17.2 || exit
   CPPFLAGS="-DCARES_STATICLIB=1" \
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
   --disable-tests \
   --disable-debug
@@ -169,14 +169,14 @@ fi
 # -----------------------------------------------------------------------------
 # build iconv
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libiconv.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libiconv.a ]; then
   wget -nc https://ftp.gnu.org/gnu/libiconv/libiconv-1.17.tar.gz
   tar -xf libiconv-1.17.tar.gz
-  cd libiconv-1.17
+  cd libiconv-1.17 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static
   (($? != 0)) && { printf '%s\n' "[iconv] configure failed"; exit 1; }
   make
@@ -188,10 +188,10 @@ fi
 # -----------------------------------------------------------------------------
 # build psl
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libpsl.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libpsl.a ]; then
   wget -nc https://github.com/rockdaboot/libpsl/releases/download/0.21.1/libpsl-0.21.1.tar.gz
   tar -xf libpsl-0.21.1.tar.gz
-  cd libpsl-0.21.1
+  cd libpsl-0.21.1 || exit
   CFLAGS="-I$INSTALL_PATH/include" \
   LIBS="-L$INSTALL_PATH/lib -lunistring -lidn2" \
   LIBIDN2_CFLAGS="-I$INSTALL_PATH/include" \
@@ -199,12 +199,12 @@ if [ ! -f $INSTALL_PATH/lib/libpsl.a ]; then
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
   --disable-gtk-doc \
   --enable-builtin=libidn2 \
   --enable-runtime=libidn2 \
-  --with-libiconv-prefix=$INSTALL_PATH
+  --with-libiconv-prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[psl] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[psl] make failed"; exit 1; }
@@ -215,14 +215,14 @@ fi
 # -----------------------------------------------------------------------------
 # build pcre2
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libpcre2-8.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libpcre2-8.a ]; then
   wget -nc https://github.com/PCRE2Project/pcre2/releases/download/pcre2-10.40/pcre2-10.40.tar.gz
   tar -xf pcre2-10.40.tar.gz
-  cd pcre2-10.40
+  cd pcre2-10.40 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static
   (($? != 0)) && { printf '%s\n' "[pcre2] configure failed"; exit 1; }
   make
@@ -234,14 +234,14 @@ fi
 # -----------------------------------------------------------------------------
 # build gpg-error
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libgpg-error.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libgpg-error.a ]; then
   wget -nc https://www.gnupg.org/ftp/gcrypt/libgpg-error/libgpg-error-1.45.tar.gz
   tar -xf libgpg-error-1.45.tar.gz
-  cd libgpg-error-1.45
+  cd libgpg-error-1.45 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
   --disable-doc
   (($? != 0)) && { printf '%s\n' "[gpg-error] configure failed"; exit 1; }
@@ -254,17 +254,17 @@ fi
 # -----------------------------------------------------------------------------
 # build assuan
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libassuan.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libassuan.a ]; then
   wget -nc https://gnupg.org/ftp/gcrypt/libassuan/libassuan-2.5.5.tar.bz2
   tar -xf libassuan-2.5.5.tar.bz2
-  cd libassuan-2.5.5
+  cd libassuan-2.5.5 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
   --disable-doc \
-  --with-libgpg-error-prefix=$INSTALL_PATH
+  --with-libgpg-error-prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[assuan] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[assuan] make failed"; exit 1; }
@@ -275,22 +275,22 @@ fi
 # -----------------------------------------------------------------------------
 # build gpgme
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libgpgme.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libgpgme.a ]; then
   wget -nc https://gnupg.org/ftp/gcrypt/gpgme/gpgme-1.17.1.tar.bz2
   tar -xf gpgme-1.17.1.tar.bz2
-  cd gpgme-1.17.1
+  cd gpgme-1.17.1 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
-  --with-libgpg-error-prefix=$INSTALL_PATH \
+  --with-libgpg-error-prefix="$INSTALL_PATH" \
   --disable-gpg-test \
   --disable-g13-test \
   --disable-gpgsm-test \
   --disable-gpgconf-test \
   --disable-glibtest \
-  --with-libassuan-prefix=$INSTALL_PATH
+  --with-libassuan-prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[gpgme] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[gpgme] make failed"; exit 1; }
@@ -301,18 +301,18 @@ fi
 # -----------------------------------------------------------------------------
 # build expat
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libexpat.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libexpat.a ]; then
   wget -nc https://github.com/libexpat/libexpat/releases/download/R_2_4_8/expat-2.4.8.tar.gz
   tar -xf expat-2.4.8.tar.gz
-  cd expat-2.4.8
+  cd expat-2.4.8 || exit
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
   --without-docbook \
   --without-tests \
-  --with-libgpg-error-prefix=$INSTALL_PATH
+  --with-libgpg-error-prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[expat] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[expat] make failed"; exit 1; }
@@ -323,18 +323,18 @@ fi
 # -----------------------------------------------------------------------------
 # build metalink
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libmetalink.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libmetalink.a ]; then
   wget -nc https://github.com/metalink-dev/libmetalink/releases/download/release-0.1.3/libmetalink-0.1.3.tar.gz
   tar -xf libmetalink-0.1.3.tar.gz
-  cd libmetalink-0.1.3
+  cd libmetalink-0.1.3 || exit
   EXPAT_CFLAGS="-I$INSTALL_PATH/include" \
   EXPAT_LIBS="-L$INSTALL_PATH/lib -lexpat" \
   ./configure \
   --host=$WGET_MINGW_HOST \
   --disable-shared \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --enable-static \
-  --with-libgpg-error-prefix=$INSTALL_PATH \
+  --with-libgpg-error-prefix="$INSTALL_PATH" \
   --with-libexpat
   (($? != 0)) && { printf '%s\n' "[metalink] configure failed"; exit 1; }
   make
@@ -346,11 +346,11 @@ fi
 # -----------------------------------------------------------------------------
 # build zlib
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libz.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libz.a ]; then
   wget -nc https://zlib.net/zlib-1.2.12.tar.gz
   tar -xf zlib-1.2.12.tar.gz
-  cd zlib-1.2.12
-  CC=$WGET_GCC ./configure --64 --static --prefix=$INSTALL_PATH
+  cd zlib-1.2.12 || exit
+  CC=$WGET_GCC ./configure --64 --static --prefix="$INSTALL_PATH"
   (($? != 0)) && { printf '%s\n' "[zlib] configure failed"; exit 1; }
   make
   (($? != 0)) && { printf '%s\n' "[zlib] make failed"; exit 1; }
@@ -361,21 +361,21 @@ fi
 # -----------------------------------------------------------------------------
 # build openssl
 # -----------------------------------------------------------------------------
-if [ ! -f $INSTALL_PATH/lib/libssl.a ]; then
+if [ ! -f "$INSTALL_PATH"/lib/libssl.a ]; then
   wget -nc https://www.openssl.org/source/openssl-1.1.1q.tar.gz
   tar -xf openssl-1.1.1q.tar.gz
-  cd openssl-1.1.1q
+  cd openssl-1.1.1q || exit
   ./Configure \
   --static \
   -static \
-  --prefix=$INSTALL_PATH \
+  --prefix="$INSTALL_PATH" \
   --cross-compile-prefix=x86_64-w64-mingw32- \
   mingw64 \
   no-shared \
   enable-asm \
   no-tests \
-  --with-zlib-include=$INSTALL_PATH \
-  --with-zlib-lib=$INSTALL_PATH/lib/libz.a
+  --with-zlib-include="$INSTALL_PATH" \
+  --with-zlib-lib="$INSTALL_PATH"/lib/libz.a
  make
  make install_sw
  cd ..
@@ -385,7 +385,7 @@ fi
 # -----------------------------------------------------------------------------
 wget -nc https://ftp.gnu.org/gnu/wget/wget-1.21.3.tar.gz
 tar -xf wget-1.21.3.tar.gz
-cd wget-1.21.3
+cd wget-1.21.3 || exit
 CFLAGS="-I$INSTALL_PATH/include -DGNUTLS_INTERNAL_BUILD=1 -DCARES_STATICLIB=1 -DPCRE2_STATIC=1 -DNDEBUG -O2 -march=$WGET_ARCH -mtune=generic" \
  LDFLAGS="-L$INSTALL_PATH/lib -static -static-libgcc" \
  GNUTLS_CFLAGS=$CFLAGS \
@@ -401,7 +401,7 @@ CFLAGS="-I$INSTALL_PATH/include -DGNUTLS_INTERNAL_BUILD=1 -DCARES_STATICLIB=1 -D
  LIBS="-L$INSTALL_PATH/lib -lhogweed -lnettle -lgmp -ltasn1 -lidn2 -lpsl -lcares -lunistring -liconv -lpcre2-8 -lmetalink -lexpat -lgpgme -lassuan -lgpg-error -lz -lcrypt32 -lpthread" \
  ./configure \
  --host=$WGET_MINGW_HOST \
- --prefix=$INSTALL_PATH \
+ --prefix="$INSTALL_PATH" \
  --disable-debug \
  --disable-valgrind-tests \
  --enable-iri \
@@ -412,16 +412,16 @@ CFLAGS="-I$INSTALL_PATH/include -DGNUTLS_INTERNAL_BUILD=1 -DCARES_STATICLIB=1 -D
  --with-cares \
  --with-libpsl \
  --with-metalink \
- --with-gpgme-prefix=$INSTALL_PATH
+ --with-gpgme-prefix="$INSTALL_PATH"
 (($? != 0)) && { printf '%s\n' "[wget gnutls] configure failed"; exit 1; }
 make clean
 make
 (($? != 0)) && { printf '%s\n' "[wget gnutls] make failed"; exit 1; }
 make install
 (($? != 0)) && { printf '%s\n' "[wget gnutls] make install"; exit 1; }
-mkdir $INSTALL_PATH/wget-gnutls
-cp $INSTALL_PATH/bin/wget.exe $INSTALL_PATH/wget-gnutls/wget-gnutls-x64.exe
-$MINGW_STRIP_TOOL $INSTALL_PATH/wget-gnutls/wget-gnutls-x64.exe
+mkdir "$INSTALL_PATH"/wget-gnutls
+cp "$INSTALL_PATH"/bin/wget.exe "$INSTALL_PATH"/wget-gnutls/wget-gnutls-x64.exe
+$MINGW_STRIP_TOOL "$INSTALL_PATH"/wget-gnutls/wget-gnutls-x64.exe
 # -----------------------------------------------------------------------------
 # build wget (openssl)
 # -----------------------------------------------------------------------------
@@ -443,7 +443,7 @@ CFLAGS="-I$INSTALL_PATH/include -DCARES_STATICLIB=1 -DPCRE2_STATIC=1 -DNDEBUG -O
  LIBS="-L$INSTALL_PATH/lib -lidn2 -lpsl -lcares -lunistring -liconv -lpcre2-8 -lmetalink -lexpat -lgpgme -lassuan -lgpg-error -lcrypto -lssl -lz -lcrypt32" \
  ./configure \
  --host=$WGET_MINGW_HOST \
- --prefix=$INSTALL_PATH \
+ --prefix="$INSTALL_PATH" \
  --disable-debug \
  --disable-valgrind-tests \
  --enable-iri \
@@ -455,12 +455,12 @@ CFLAGS="-I$INSTALL_PATH/include -DCARES_STATICLIB=1 -DPCRE2_STATIC=1 -DNDEBUG -O
  --with-libpsl \
  --with-metalink \
  --with-openssl \
- --with-gpgme-prefix=$INSTALL_PATH
+ --with-gpgme-prefix="$INSTALL_PATH"
 (($? != 0)) && { printf '%s\n' "[wget openssl] configure failed"; exit 1; }
 make
 (($? != 0)) && { printf '%s\n' "[wget openssl] make failed"; exit 1; }
 make install
 (($? != 0)) && { printf '%s\n' "[wget openssl] make install"; exit 1; }
-mkdir $INSTALL_PATH/wget-openssl
-cp $INSTALL_PATH/bin/wget.exe $INSTALL_PATH/wget-openssl/wget-openssl-x64.exe
-$MINGW_STRIP_TOOL $INSTALL_PATH/wget-openssl/wget-openssl-x64.exe
+mkdir "$INSTALL_PATH"/wget-openssl
+cp "$INSTALL_PATH"/bin/wget.exe "$INSTALL_PATH"/wget-openssl/wget-openssl-x64.exe
+$MINGW_STRIP_TOOL "$INSTALL_PATH"/wget-openssl/wget-openssl-x64.exe
